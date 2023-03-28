d8b4370ff5f497a70d1cbe7dcdd8b3cd2c503038
Urs-Prem <122676954+Urs-Prem@users.noreply.github.com>
2023-03-28 23:16:30 +0530

    Create main.js

---
+++ Todo List/main.js
@@ -0,0 +1,32 @@
+let button = document.getElementById('add');
+
+let container = document.getElementById('todocontainer');
+
+let inputField = document.getElementById('input')
+
+button.addEventListener('click', function(){
+  let paragraph = document.createElement('p')
+  
+  paragraph.classList.add('paragraph-styling');
+  
+  
+  paragraph.innerText = inputField.value;
+  
+  
+  container.appendChild(paragraph);
+  inputField.value ='';
+  
+  
+  
+  paragraph.addEventListener('click', function(){
+    paragraph.style.textDecoration ='line-through';
+    paragraph.style.color='red';
+  })
+  
+  paragraph.addEventListener('dblclick', function(){
+    container.removeChild(paragraph);
+  })
+  
+  
+  
+})
